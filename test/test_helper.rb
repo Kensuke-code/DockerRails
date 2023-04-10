8f8322c4b464f64d770b0f6963da9c1eebbc0bfb
Kensuke-code <kenzo71719083@gmail.com>
2023-04-11 07:32:25 +0900

    実装完了
    staticpages実装完了
    rspecでの統合テスト

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,12 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  include ApplicationHelper
 end
+
+
+# note: rspecのテストコマンド
+# dc exec web bundle exec rspec --profile 10
+
+# note : 結合テスト
+# dc exec web bundle exec rspec spec/system/static_pages_spec.rb
