e423a3cfc16a27fbc523b70d8aa6c2b48f665a15
Kensuke-code <kenzo71719083@gmail.com>
2023-04-15 14:55:20 +0900

    gem追加
    byebugとannotate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  email      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50}
-  validates :email, presence: true, length: { maximum: 255}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255}, format: { with: VALID_EMAIL_REGEX}
 end
