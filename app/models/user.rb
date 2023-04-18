0bab7d6616666e0f032f791b2115729ae15b9105
Kensuke-code <kenzo71719083@gmail.com>
2023-04-19 07:30:44 +0900

    annotateの自動化、password_digestカラム追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,12 @@
 #
 # Table name: users
 #
-#  id         :bigint           not null, primary key
-#  name       :string
-#  email      :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :bigint           not null, primary key
+#  email           :string
+#  name            :string
+#  password_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
   # 保存前のcallback関数
@@ -15,4 +16,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255}, format: { with: VALID_EMAIL_REGEX}, uniqueness: true
+
+  has_secure_password
+
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,15 +2,18 @@
 #
 # Table name: users
 #
-#  id         :bigint           not null, primary key
-#  name       :string
-#  email      :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :bigint           not null, primary key
+#  email           :string
+#  name            :string
+#  password_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
 #
 FactoryBot.define do
   factory :user do
     name { Faker::Name.name }
     email { Faker::Internet.email }
+    password  {"0419test"}
+    password_confirmation  {"0419test"}
   end
 end
